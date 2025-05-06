c318879da8a063f9f9884a8c6794bfa6ac607f27
ranggarap <fadhilahrangga26@gmail.com>
2025-05-06 10:51:58 +0700

    membuat fungsi void dan add note

--- singly_linkedist.cpp
+++ singly_linkedist.cpp
@@ -1,4 +1,32 @@
 #include <iostream>
 #include <string.h>
-using namespace std;
+using namespace std;class Node
+{
+public:
+    int noMhs;
+    Node *next;
+};
 
+class LinkedList
+{
+    Node*START;
+public :
+    LinkedList()
+    {
+        START = NULL;
+    }
+    void addNote()
+    {
+        int nim ;
+        cout << "\nMasukan Nomor Mahasiswa:";
+        cin >> nim;
+        
+        Node *nodeBaru =new Node;
+        nodeBaru->noMhs =nim;
+        
+        if (START == NULL || nim <= START->noMhs)
+    {
+        
+    }
+    }
+};
